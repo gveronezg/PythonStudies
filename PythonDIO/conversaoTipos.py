086c39c084dd11a197d03a2279e77034e7c04e31
gveronezg <gabrielveronezgiolo@gmail.com>
2023-07-11 12:10:13 -0300

    subindo aula(Conversao de tipos)

--- PythonDIO/conversaoTipos.py
+++ PythonDIO/conversaoTipos.py
@@ -1,1 +1,28 @@
-"Save"
+"""
+        Tipos built-in são:
+
+Texto       =   str
+Númerico    =   int, float, complex
+Sequência   =   list, tuple, range
+Mapa        =   dict
+Coleção     =   set, fronzenset
+Booleano    =   bool
+Binário     =   bytes, bytearray, memoryview
+
+"""
+
+print(2+3)
+print(1.3+2.7)
+print(True)
+print(False)
+print("String")
+
+int()
+float()
+str()
+bool()
+
+"""
+    NO TERMINAL
+entre com "python3" para ativar o modo interativo
+"""
