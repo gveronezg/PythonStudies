0fae9e14e6307cfeb34bab019c35e32c32021f37
gveronezg <gabrielveronezgiolo@gmail.com>
2023-07-11 12:37:45 -0300

    subindo aulas

---
+++ PythonDIO/funcoesEntrada&Saida.py
@@ -0,0 +1,8 @@
+nome = input("Entre com seu nome: ")
+sobrenome = input("Entre com seu sobrenome: ")
+
+print(nome, sobrenome)
+"parametro (end) ao final do print, insere o dado por ultimo"
+print(nome, sobrenome, end="...\n")
+"parametro (sep) ao final do print, insere o dado entre os parâmetros"
+print(nome, sobrenome, sep="_")
---
+++ PythonDIO/operadoresAritméticos.py
@@ -0,0 +1,22 @@
+"Adição"
+print(2+2)
+"Subtração"
+print(2-2)
+"Multiplicação"
+print(2*2)
+"Divisão/float"
+print(2/2)
+"Divisão/int"
+print(2//2)
+"Módulo"
+print(2%2)
+"Exponenciação"
+print(2**2)
+
+"""
+Precedência dos operadores
+1º Parêntesis
+2º Expoentes
+3º Multiplicações e divisões (da esquerda para a direita)
+4º Somas e subtrações (da esquerda para a direita)
+"""
